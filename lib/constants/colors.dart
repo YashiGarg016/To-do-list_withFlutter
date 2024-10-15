13b47d1cf2ecb510bfeb12f2c2b2616a28d4a72b
Yashi Garg <yashigarg016@gmail.com>
2024-10-15 15:44:20 +0530

    Initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFE6A4B4);//delete box
+const Color tdBlue = Color(0xFFE6A4B4);//tick box, plus task
+const Color tdBlack = Color(0xFF45474B);//all the text, search symbol
+const Color tdGrey = Color(0xFFE6A4B4);//search icon
+const Color tdBgColor = Color(0xFFFFF8E3);//bg duh
